f93133059b90f3c332e8a37f7e9db0a54b01bc58
VirginiaTsang <virginiacytsang@gmail.com>
2023-11-05 16:55:04 +0800

    add random

--- src/main/java/org/Comp3111F23G7/FunctionB/Searcher.java
+++ src/main/java/org/Comp3111F23G7/FunctionB/Searcher.java
@@ -2,6 +2,9 @@ package org.Comp3111F23G7.FunctionB;
 
 import org.Comp3111F23G7.Vertex;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Searcher {
 
     private int[][] maze_matrix;
@@ -11,6 +14,7 @@ public class Searcher {
     }
 
     public void Bfs(int[][] maze_matrix, Vertex tom_loc, Vertex jerry_loc){
+        Queue<Vertex> q = new LinkedList<>();
 
     }
     public void Dfs(int[][] maze_matrix, Vertex tom_loc, Vertex jerry_loc){
