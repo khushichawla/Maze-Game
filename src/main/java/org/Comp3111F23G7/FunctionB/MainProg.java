f51e8afcd1cd7b7bed181074b744328b49ca5480
VirginiaTsang <virginiacytsang@gmail.com>
2023-11-10 13:33:13 +0800

    Writing Junit5 test case

--- src/main/java/org/Comp3111F23G7/FunctionB/MainProg.java
+++ src/main/java/org/Comp3111F23G7/FunctionB/MainProg.java
@@ -3,6 +3,8 @@ package org.Comp3111F23G7.FunctionB;
 import org.Comp3111F23G7.Vertex;
 
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
 
 public class MainProg {
     public static void main(String[] args){
@@ -24,7 +26,7 @@ public class MainProg {
                         {0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,1,1,1,1,1,0,1,1,0,0,1,0,0,1,1},
                         {0,1,0,1,1,1,1,0,1,0,0,0,0,1,1,1,0,0,0,0,0,1,0,1,1,1,1,1,0,1},
                         {1,1,1,1,0,0,0,0,1,1,1,1,0,1,0,0,0,1,1,1,0,1,0,1,0,0,0,0,0,1},
-                        {1,0,0,1,1,1,0,0,0,0,0,1,0,0,0,1,0,1,0,1,1,1,0,1,1,0,1,1,1,1},
+                        {1,0,0,1,1,1,0,1,0,0,0,1,0,0,0,1,0,1,0,1,1,1,0,1,1,0,1,1,1,1},
                         {1,1,0,0,0,1,1,0,0,1,0,1,0,0,1,1,0,0,0,0,0,0,0,0,1,0,1,0,1,0},
                         {0,1,1,0,0,0,1,1,0,1,0,1,1,1,1,0,0,0,1,1,1,1,1,0,1,0,1,0,1,1},
                         {1,0,1,1,1,1,0,1,0,1,0,0,0,0,0,0,0,1,1,0,1,0,0,0,1,0,1,0,0,1},
@@ -39,7 +41,35 @@ public class MainProg {
                         {1,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,0,1,1,1,1,0}};
         Searcher s =  new Searcher(matrix);
         Vertex[] path = s.dijkstra(new Vertex(0,12), new Vertex(29,1));
-        System.out.println("hello");
+        int shortestpathlen = path.length;
+        List<Vertex[]> altpaths = s.findDistinctPaths(new Vertex(0,12), new Vertex(29,1),5);
+        s.colorMazeWithPath(path, 2);
+        Iterator<Vertex[]> itr = altpaths.iterator();
+        while (itr.hasNext()) {
+            System.out.println("there is A path");
+            Vertex[] p = itr.next();
+            System.out.println(p.length);
+            if (p.length == shortestpathlen) {
+                s.colorMazeWithPath(p, 2);
+                itr.remove();
+            }
+        }
+        itr = altpaths.listIterator();
+        while (itr.hasNext()) {
+            System.out.println("there is B path");
+            Vertex[] p = itr.next();
+            System.out.println(p.length);
+            s.colorMazeWithPath(p, 5);
+        }
+        System.out.println(shortestpathlen);
+
+        try{
+            s.outputMaze();
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+
         for(Vertex v :path){
             System.out.print("[");
             System.out.print(v.getX());
@@ -47,10 +77,19 @@ public class MainProg {
             System.out.print(v.getY());
             System.out.print("]");
         }
-        try {
-            s.outputMazeWithPath(path);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        System.out.println("\nfound shortest path");
+
+
+        for (Vertex[] i : altpaths){
+            System.out.println("\nThe path: ");
+            for(Vertex v :i){
+                System.out.print("[");
+                System.out.print(v.getX());
+                System.out.print(" ");
+                System.out.print(v.getY());
+                System.out.print("]");
+            }
         }
+        System.out.println("\nfound paths");
     }
 }
--- src/main/java/org/Comp3111F23G7/FunctionB/Searcher.java
+++ src/main/java/org/Comp3111F23G7/FunctionB/Searcher.java
@@ -1,23 +1,30 @@
 package org.Comp3111F23G7.FunctionB;
-
 import org.Comp3111F23G7.Vertex;
 
+import java.io.IOException;
 import java.util.*;
 import java.io.FileWriter;
-import java.io.IOException;
-import java.security.spec.ECField;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Searcher {
-
     private int[][] maze_matrix;
+    private int[][] output_matrix;
 
     public Searcher(int[][] maze_matrix) {
         this.maze_matrix = maze_matrix;
+        this.output_matrix = new int[30][30];
+        for (int i = 0; i< maze_matrix.length; ++i){
+            for (int j = 0; j< maze_matrix.length; ++j) {
+                if (maze_matrix[j][i] == 0) {
+                    output_matrix[j][i] = 1;
+                } else
+                    output_matrix[j][i] = 4;
+            }
+        }
     }
 
-    public Vertex[] Bfs(Vertex start, Vertex end) {
+    public Vertex[] bfs(Vertex start, Vertex end) {
         if (maze_matrix[start.getY()][start.getX()] == 1 || maze_matrix[end.getY()][end.getX()] == 1) {
             return new Vertex[0]; // Start or end is a wall
         }
@@ -77,11 +84,11 @@ public class Searcher {
         }
         distances[start.getY()][start.getX()] = 0;
 
+        // Initialize the prev array to store the previous vertex on the shortest path
+        Vertex[][] prev = new Vertex[maze_matrix.length][maze_matrix[0].length];
+
         // Priority queue to keep track of vertices to be evaluated
         PriorityQueue<NodeState> queue = new PriorityQueue<>(Comparator.comparingInt(ns -> distances[ns.vertex.getY()][ns.vertex.getX()]));
-
-        // Initialize prev array to reconstruct the path later
-        Vertex[][] prev = new Vertex[maze_matrix.length][maze_matrix[0].length];
 
         // Add the start vertex to the queue
         queue.add(new NodeState(start, null));
@@ -106,7 +113,8 @@ public class Searcher {
                 }
 
                 // Calculate new distance
-                int newDist = distances[currentVertex.getY()][currentVertex.getX()] + 1;
+                int edgeWeight = maze_matrix[neighborY][neighborX] == 0 ? 1 : maze_matrix[neighborY][neighborX]; // Consider non-wall cells as having a weight of 1
+                int newDist = distances[currentVertex.getY()][currentVertex.getX()] + edgeWeight;
                 if (newDist < distances[neighborY][neighborX]) {
                     distances[neighborY][neighborX] = newDist;
                     prev[neighborY][neighborX] = currentVertex;
@@ -117,46 +125,107 @@ public class Searcher {
 
         // Reconstruct the path from end to start
         List<Vertex> path = new ArrayList<>();
-        for (Vertex at = end; at != null && prev[at.getY()][at.getX()] != null; at = prev[at.getY()][at.getX()]) {
-            path.add(at);
+        Vertex at = end;
+        if (prev[at.getY()][at.getX()] != null || at.equals(start)) {
+            while (at != null) {
+                path.add(at);
+                at = prev[at.getY()][at.getX()];
+            }
         }
-        path.add(start); // Add the start vertex
-
         Collections.reverse(path);
 
         // Convert to Vertex array
         return path.toArray(new Vertex[0]);
     }
 
-    public void outputMazeWithPath(Vertex[] path) throws IOException {
-        // Copy maze matrix
-        int[][] outputMatrix = new int[30][30];
-        for (int i = 0; i< maze_matrix.length; ++i){
-            for (int j = 0; j< maze_matrix.length; ++j){
-                if (maze_matrix[j][i] == 0){
-                    outputMatrix[j][i] = 1;
-                }
-                else
-                    outputMatrix[j][i] = 4;
+        private class Path {
+            List<Vertex> vertices = new ArrayList<>();
+            Set<Vertex> visited = new HashSet<>();
+
+            Path() {
+            }
+
+            Path(Path other) {
+                this.vertices = new ArrayList<>(other.vertices);
+                this.visited = new HashSet<>(other.visited);
+            }
+
+            void add(Vertex v) {
+                vertices.add(v);
+                visited.add(v);
+            }
+
+            boolean contains(Vertex v) {
+                return visited.contains(v);
+            }
+
+            Vertex[] toArray() {
+                return vertices.toArray(new Vertex[0]);
             }
         }
+
+        // Method to find multiple distinct paths
+        public List<Vertex[]> findDistinctPaths(Vertex start, Vertex end, int numberOfPaths) {
+            List<Vertex[]> distinctPaths = new ArrayList<>();
+            findPathsDFS(start, end, new Path(), distinctPaths, numberOfPaths);
+            return distinctPaths;
+        }
+
+        private void findPathsDFS(Vertex current, Vertex end, Path currentPath, List<Vertex[]> paths, int numberOfPaths) {
+            // Add the current vertex to the path
+            currentPath.add(current);
+
+            if (current.equals(end)) {
+                // Found a path
+                paths.add(currentPath.toArray());
+                if (paths.size() == numberOfPaths) {
+                    // Found the required number of paths
+                    return;
+                }
+            } else {
+                // Explore neighbors
+                for (int[] direction : new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}) { // Directions: down, right, up, left
+                    int newX = current.getX() + direction[0];
+                    int newY = current.getY() + direction[1];
+                    Vertex neighbor = new Vertex(newX, newY);
+
+                    if (isValidStep(neighbor) && !currentPath.contains(neighbor)) {
+                        // Continue the path with the neighbor
+                        findPathsDFS(neighbor, end, new Path(currentPath), paths, numberOfPaths);
+                        if (paths.size() == numberOfPaths) {
+                            // If the number of required paths has been found, stop searching
+                            return;
+                        }
+                    }
+                }
+            }
+            // Remove the current vertex from the path when backtracking
+            currentPath.vertices.remove(currentPath.vertices.size() - 1);
+            currentPath.visited.remove(current);
+        }
+
+        // Check if the step is within bounds and not a wall
+        private boolean isValidStep(Vertex v) {
+            return v.getX() >= 0 && v.getX() < maze_matrix[0].length && v.getY() >= 0 && v.getY() < maze_matrix.length && maze_matrix[v.getY()][v.getX()] == 0;
+        }
+
+
+    public void colorMazeWithPath(Vertex[] path, int color){
         for (Vertex v: path){
             int x_loc = v.getX();
             int y_loc = v.getY();
-            outputMatrix[y_loc][x_loc] = 2;
+            output_matrix[y_loc][x_loc] = color;
         }
+    }
 
-        // Write to file
+    public void outputMaze() throws IOException {
         try (FileWriter writer = new FileWriter("maze_output.txt")) {
-            for (int[] row : outputMatrix) {
+            for (int[] row : output_matrix) {
                 for (int cell : row) {
                     writer.write(Integer.toString(cell));
                 }
                 writer.write("\n");
             }
-        }
-        catch (Exception e){
-            System.out.print(e);
         }
     }
 
