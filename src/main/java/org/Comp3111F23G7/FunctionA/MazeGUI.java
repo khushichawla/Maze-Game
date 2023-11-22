790ca93975c46caef637c2d1c8b38a67433a1e33
khushichawla <khushichawlaemails@gmail.com>
2023-11-23 01:36:47 +0800

    Modified MazeGUI to update the same frame instead of creating a new frame everytime.

--- src/main/java/org/Comp3111F23G7/FunctionA/MazeGUI.java
+++ src/main/java/org/Comp3111F23G7/FunctionA/MazeGUI.java
@@ -1,16 +1,29 @@
 package org.Comp3111F23G7.FunctionA;
 
+import org.Comp3111F23G7.KeyboardListener;
+import org.Comp3111F23G7.ThreadsController;
+import org.Comp3111F23G7.Vertex;
+
 import javax.swing.*;
 import java.awt.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * MazeGUI class
  */
 public class MazeGUI {
     public static boolean isVisible = false;
+    public static JFrame frame = new JFrame("Maze Game");
+    private static JPanel[][] mazePanels;
+    private static int[][] playerLocations;
+    private static int[][] maze;
+
+    public MazeGUI(int[][] maze) {
+        this.maze = maze;
+    }
 
     /**
      * Main function
@@ -66,35 +79,20 @@ public class MazeGUI {
      * @param maze - int 2D array of maze
      */
     public static void createAndShowMazeGUI(int[][] maze) {
-//        if (maze.length != 30) {
-//            isVisible = false;
-//            System.out.println("Invalid maze: Maze is null");
-//            return;
-//        }
         int rows = maze.length;
         int cols = maze[0].length;
 
-        JFrame frame = new JFrame("Maze Game");
+//        JFrame frame = new JFrame("Maze Game");
+        frame.getContentPane().removeAll();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new GridLayout(rows, cols));
+        mazePanels = new JPanel[rows][cols];
 
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < cols; col++) {
                 JPanel panel = new JPanel();
-                if (maze[row][col] == 2) {
-                    panel.setBackground(Color.BLUE);
-                } else if (maze[row][col] == 7) {
-                    panel.setBackground(Color.GREEN);
-                } else if (maze[row][col] == 3) {
-                    panel.setBackground(Color.RED);
-                } else if (maze[row][col] == 0) {
-                    panel.setBackground(Color.WHITE);
-                } else if (maze[row][col] == 4) {
-                    panel.setBackground(Color.BLACK);
-                } else if (maze[row][col] == 5) {
-                    panel.setBackground(Color.YELLOW);
-                } else panel.setBackground(Color.GRAY);
-                // panel.setBackground(maze[row][col] == 0 ? Color.GRAY : Color.WHITE);
+                panel.setBackground(getCellColor(maze[row][col]));
+                mazePanels[row][col] = panel;
                 frame.add(panel);
             }
         }
@@ -102,6 +100,31 @@ public class MazeGUI {
         isVisible = true;
         frame.pack();
         frame.setVisible(true);
+        frame.revalidate(); // Refresh the frame
+        frame.repaint();
+    }
+
+    private static Color getCellColor(int cellValue) {
+        switch (cellValue) {
+            case 0:
+                return Color.WHITE; // Clear path
+            case 2:
+                return Color.BLUE; // Shortest Path
+            case 3:
+                return Color.RED; // Exit Point
+            case 4:
+                return Color.BLACK; // Maze Border
+            case 5:
+                return Color.YELLOW; // All Possible Paths
+            case 6:
+                return Color.ORANGE; // Jerry
+            case 7:
+                return Color.GREEN; // Entry Point
+            case 8:
+                return Color.PINK; // Tom
+            default:
+                return Color.GRAY; // Wall
+        }
     }
 
     public boolean isGUIVisible() {
